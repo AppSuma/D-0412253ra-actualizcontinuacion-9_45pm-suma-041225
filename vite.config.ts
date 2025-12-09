fd1a971b4aefaaa1f46638594854dc4b1ff23c43
AppSuma <teducadigital@gmail.com>
2025-12-08 23:55:00 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite'; // No necesitamos loadEnv si usamos el formato VITE_ estándar
+import { defineConfig } from 'vite'; 
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
         host: '0.0.0.0',
     },
     plugins: [react()],
-    // ELIMINAMOS EL BLOQUE 'define' para usar el formato estándar de Vite (import.meta.env.VITE_...)
+    // Corrección: Eliminamos la sección 'define' para usar el formato estándar de Vercel/Vite (import.meta.env.VITE_...)
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '.'),
